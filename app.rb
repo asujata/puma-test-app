c152e31b4ffe3e02f89ecf429f38483b919b576c
asujata <andreas.sujata@sitrox.com>
2021-08-19 16:46:54 +0200

    update

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require 'sinatra'
 get '/' do
   require 'socket'
   hostname = Socket.gethostname
-  response = ""
+  response = "================ REQUEST ENV ================<br/>"
   request.env.each do |k, v|
+    response << "#{k}: #{v}<br/>"
+  end
+  response << "<br/>================ HOST ENV ================<br/>"
+  ENV.each do |k, v|
     response << "#{k}: #{v}<br/>"
   end
   response
